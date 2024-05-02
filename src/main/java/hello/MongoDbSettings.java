50b8a184df7740c554e5d3812716390279cfed5c
andrey.yuldashev <andrey.yuldashev@jetbrains.com>
2024-05-02 18:40:12 +0300

    Fix old repository by moving to Gradle 7.3 and Java 17
    Java 17 is supported starting from Gradle 7.3

--- src/main/java/hello/MongoDbSettings.java
+++ src/main/java/hello/MongoDbSettings.java
@@ -1,20 +1,23 @@
 package hello;
 
-import com.mongodb.MongoClientOptions;
+import com.mongodb.MongoClientSettings;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
+import java.util.concurrent.TimeUnit;
 
 @Configuration
 public class MongoDbSettings {
     static class OptionsConfig {
 
         @Bean
-        public MongoClientOptions mongoOptions() {
-            return MongoClientOptions.builder()
-                    .socketTimeout(2000)
-                    .connectTimeout(2000)
-                    .heartbeatSocketTimeout(2000)
-                    .heartbeatConnectTimeout(2000)
+        public MongoClientSettings mongoClientSettings() {
+            return MongoClientSettings.builder()
+                    .applyToSocketSettings(builder -> builder
+                            .connectTimeout(2, TimeUnit.SECONDS)
+                            .readTimeout(2, TimeUnit.SECONDS))
+                    .applyToClusterSettings(builder -> builder
+                            .serverSelectionTimeout(2, TimeUnit.SECONDS))
                     .build();
         }
 
